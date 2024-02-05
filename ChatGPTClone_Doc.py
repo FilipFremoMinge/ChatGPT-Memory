3a9e8523b4b0290161d3d0a5fcd2cd4f9e44ebf6
FilipFremoMinge <157702246+FilipFremoMinge@users.noreply.github.com>
2024-02-05 20:40:26 +0100

    Update ChatGPTClone_Doc.py

--- ChatGPTClone_Doc.py
+++ ChatGPTClone_Doc.py
@@ -61,7 +61,7 @@ st.markdown(
 
 
 # Initialiser ChatOpenAI-modellen
-API = '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'
+API = st.secrets["API_KEY"]
 chat = ChatOpenAI(temperature=0.5, model_name="gpt-4-0125-preview", openai_api_key=API, verbose=True)
 
 # Initialiser session state for å lagre meldinger
